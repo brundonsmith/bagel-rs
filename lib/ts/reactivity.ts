24eaa218d2707d8dd458f73f8cd26c82c7406bfb
Brandon Smith <mail@brandons.me>
2023-03-23 22:57:23 -0500

    Rewrote reactivity system

--- lib/ts/reactivity.ts
+++ lib/ts/reactivity.ts
@@ -1,277 +1,306 @@
 
-export const WHOLE_OBJECT = Symbol('WHOLE_OBJECT')
-const COMPUTED_RESULT = Symbol('COMPUTED_RESULT')
+// --- Utilities ---
 
-class NMap<TValue> {
-    private map = new Map<any, any>()
+class IterableWeakMap<K extends object, V extends object> {
+    private weakMap = new WeakMap<K, V>()
+    private map = new Map<string | number | boolean, V>()
+    private refSet = new Set<WeakRef<K>>()
 
-    get(keys: unknown[]): TValue | undefined {
-        let current = { rest: this.map, value: undefined as TValue | undefined }
-
-        for (let i = 0; i < keys.length; i++) {
-            if (current == null) {
-                return undefined
+    set(key: K, value: V) {
+        if (typeof key === 'object') {
+            if (!this.weakMap.has(key)) {
+                this.refSet.add(new WeakRef(key))
             }
 
-            current = current.rest.get(keys[i])
-        }
-
-        return current?.value
-    }
-
-    set(keys: unknown[], value: TValue) {
-        let current = { rest: this.map, value: undefined as unknown }
-
-        for (let i = 0; i < keys.length; i++) {
-            let next = current.rest.get(keys[i])
-
-            if (next == null) {
-                next = { rest: new Map() }
-                current.rest.set(keys[i], next)
-            }
-
-            current = next
-        }
-
-        current.value = value
-    }
-
-    entries() {
-        function* nmapEntries(argsSoFar: unknown[], current: { rest: Map<any, any>, value?: TValue }): Generator<[unknown[], TValue]> {
-            if (Object.hasOwn(current, 'value')) {
-                yield [argsSoFar, current.value as TValue]
-            }
-
-            for (const [arg, obj] of current.rest.entries()) {
-                yield* nmapEntries([...argsSoFar, arg], obj)
-            }
-        }
-
-        return nmapEntries([], { rest: this.map })
-    }
-}
-
-type Reaction = () => void | Promise<void>
-type Observable = { obj: WeakRef<object>, prop: string | number | typeof COMPUTED_RESULT | typeof WHOLE_OBJECT }
-
-let reportObservableAccessed: ((obs: Observable) => void) | undefined;
-let queuedReactions: Set<Reaction> | undefined // if defined, we're in an action
-let trigger: { readonly obj: object, readonly prop: string | number | typeof COMPUTED_RESULT | typeof WHOLE_OBJECT } | undefined
-let observablesToReactions: Array<
-    Observable & { effect: Reaction }
-> = []
-const memoCache = new NMap<MemoCacheEntry>()
-type MemoCacheEntry = {
-    observables: Observable[]
-    cached: unknown | typeof EMPTY_CACHE
-}
-const EMPTY_CACHE = Symbol('EMPTY_CACHE')
-
-// @ts-ignore
-export function observe<O extends object, K extends (keyof O & string | number)>(obj: O, prop: K): O[K];
-export function observe<O extends object, K extends typeof WHOLE_OBJECT>(obj: O, prop: K): O;
-export function observe<O extends object, K extends typeof COMPUTED_RESULT>(obj: O, prop: K): undefined;
-export function observe<O extends object, K extends (keyof O & string | number) | typeof COMPUTED_RESULT | typeof WHOLE_OBJECT>(obj: O, prop: K)
-    // @ts-ignore
-    : O[K] | O | undefined {
-
-    // if something is observing, report this obj/prop to it
-    if (reportObservableAccessed && obj != null) {
-        reportObservableAccessed({ obj: new WeakRef(obj), prop })
-    }
-
-    // return observed value
-    if (prop === WHOLE_OBJECT) {
-        return obj
-    } else if (prop === COMPUTED_RESULT) {
-        return undefined
-    } else {
-        // @ts-ignore
-        const val = obj?.[prop]
-
-        if (typeof val === 'function') {
-            return val.bind(obj) // methods
+            this.weakMap.set(key, value)
         } else {
-            return val
-        }
-    }
-}
-
-export function invalidate(obj: object, prop: string | number | typeof COMPUTED_RESULT | typeof WHOLE_OBJECT = WHOLE_OBJECT, value?: any) {
-    if (arguments.length === 3 && obj != null) {
-        if ((obj as any)[prop as any] === value) {
-            // if new value is the same as old value, don't invalidate
-            return
-        } else if (prop !== WHOLE_OBJECT && prop !== COMPUTED_RESULT) {
-            (obj as any)[prop as any] = value
+            this.map.set(key, value)
         }
     }
 
-    const topOfAction = queuedReactions == null
-    queuedReactions = queuedReactions ?? new Set()
-    // TODO: Don't invalidate if the new value is the same as the previous value
-    if (topOfAction) trigger = { obj, prop }
+    has(key: K) {
+        return this.weakMap.has(key) || this.map.has(key)
+    }
 
-    // invalidate cached function results that observed this obj/prop
-    for (const [args, entry] of memoCache.entries()) {
-        const entryIsInvalidated =
-            entry.observables.some(observable =>
-                observable.obj.deref() === obj && (prop === WHOLE_OBJECT || observable.prop === WHOLE_OBJECT || prop === observable.prop))
+    get(key: K) {
+        return this.weakMap.get(key) ?? this.map.get(key)
+    }
 
-        if (entryIsInvalidated) {
-            const alreadyInvalidated = entry.cached === EMPTY_CACHE
-            entry.cached = EMPTY_CACHE
-            if (!alreadyInvalidated) {
-                const fn = args[0] as object
-                invalidate(fn, COMPUTED_RESULT)
+    delete(key: K) {
+        if (this.weakMap.has(key)) {
+            this.weakMap.delete(key)
+            this.map.delete(key)
+            // this.refSet = new Set([...this.refSet].filter(el => el.deref() !== undefined && el.deref() !== key))
+            for (const el of this.refSet) {
+                if (el.deref() === undefined || el.deref() === key) {
+                    this.refSet.delete(el)
+                }
             }
         }
     }
 
-    // queue up reactions that observed this obj/prop
-    for (const entry of observablesToReactions) {
-        if (obj === entry.obj.deref() && (prop === WHOLE_OBJECT || entry.prop === WHOLE_OBJECT || prop === entry.prop)) {
-            queuedReactions.add(entry.effect)
-        }
+    clear() {
+        this.weakMap = new WeakMap<K, V>()
+        this.map = new Map<string | number | boolean, V>()
+        this.refSet = new Set<WeakRef<K>>()
     }
 
-    // run queued reactions
-    if (topOfAction) {
-        for (const effect of queuedReactions) {
-            effect()
+    *[Symbol.iterator]() {
+        for (const keyRef of this.refSet) {
+            const key = keyRef.deref()
+
+            if (key !== undefined) {
+                const value = this.weakMap.get(key)
+
+                if (value !== undefined) {
+                    yield [key, value] as const
+                }
+            }
         }
 
-        if (topOfAction) trigger = undefined
-
-        queuedReactions = undefined
+        for (const [key, value] of this.map) {
+            yield [key, value] as const
+        }
     }
 }
 
-type MemoOptions = {
-    maxItems?: number
-}
+const VALUE_ITSELF = Symbol('VALUE_ITSELF')
 
-export function memo<F extends Function>(fn: F, options: MemoOptions = {}): F {
-    // TODO: Respond to options
+class WeakTupleSet<TKeys extends readonly any[]> {
+    private readonly baseMap = new IterableWeakMap<any, any>()
 
-    const computed = ((...args: any[]) => {
-        observe(fn, COMPUTED_RESULT)
+    add(keys: TKeys) {
+        let currentMap = this.baseMap
 
-        // see if we have a cache entry for this function already
-        const cacheEntry = memoCache.get([fn, ...args])
+        for (const key of keys) {
+            if (!currentMap.has(key)) {
+                currentMap.set(key, new IterableWeakMap())
+            }
 
-        // if we have a valid cache entry, just return it
-        if (cacheEntry && cacheEntry.cached !== EMPTY_CACHE) {
-            return cacheEntry.cached
-        } else {
+            currentMap = currentMap.get(key)
+        }
 
-            // call the function and collect the observables that were accessed
-            const previous = reportObservableAccessed
-            const observables: Observable[] = []
-            reportObservableAccessed = obs => observables.push(obs)
-            const result = fn(...args)
-            reportObservableAccessed = previous
+        currentMap[VALUE_ITSELF] = true
+    }
 
-            // cache the result alongside the observables
-            if (!cacheEntry) {
-                memoCache.set([fn, ...args], {
-                    observables,
-                    cached: result
-                })
+    has(keys: TKeys): boolean {
+        for (const _ of this.get(keys)) {
+            return true // has at least one
+        }
+
+        return false
+    }
+
+    delete(keys: readonly any[]) {
+        let current = this.baseMap
+
+        for (const key of keys) {
+            if (current instanceof IterableWeakMap) {
+                current = current.get(key)
             } else {
-                cacheEntry.observables = observables
-                cacheEntry.cached = result
-            }
-
-            return result
-        }
-    }) as unknown as F
-
-    Object.defineProperty(computed, 'name', { value: fn.name, writable: false });
-
-    return computed
-}
-
-export function autorun(fn: Reaction, until: (() => boolean) | undefined) {
-    function effect() {
-        const newObservablesToReactions: typeof observablesToReactions = []
-
-        // run the reaction and collect all new mappings
-        const previous = reportObservableAccessed
-        reportObservableAccessed = obs => newObservablesToReactions.push({ ...obs, effect })
-
-        if (until != null) {
-            const finished = until()
-            if (finished) {
-                unsubscribe()
                 return
             }
         }
 
-
-        fn()
-        reportObservableAccessed = previous
-
-        observablesToReactions = [
-            // remove this reaction's entries from the mapping    
-            ...observablesToReactions.filter(r => r.effect !== effect),
-            ...newObservablesToReactions
-        ]
+        if (current instanceof IterableWeakMap) {
+            delete current[VALUE_ITSELF]
+            current.clear()
+        }
     }
 
-    Object.defineProperty(effect, 'name', { value: fn.name, writable: false });
+    * get(keys: readonly any[] = []) {
+        let currentMap = this.baseMap
 
-    effect()
+        for (const key of keys) {
+            if (!currentMap.has(key)) {
+                return
+            }
 
-    const unsubscribe = () => {
-        // unsubscribe
-        observablesToReactions = observablesToReactions.filter(r => r.effect !== effect)
+            currentMap = currentMap.get(key)
+        }
+
+        if (currentMap instanceof IterableWeakMap) {
+            yield* iterateWeakMapRecursive(currentMap)
+        }
+    }
+}
+
+function* iterateWeakMapRecursive(map: IterableWeakMap<any, any>): Generator<any[]> {
+    for (const [key, value] of map) {
+        if (value[VALUE_ITSELF]) {
+            yield [key]
+        }
+
+        if (value instanceof IterableWeakMap) {
+            for (const rest of iterateWeakMapRecursive(value)) {
+                yield [key, ...rest]
+            }
+        }
+    }
+}
+
+// // --- Core state ---
+
+type Observable = object
+type PropertyName = string
+type Reaction = () => void | Promise<void>;
+type Computation = (...args: unknown[]) => unknown;
+
+let reportObservableAccessed: (<TObservable extends Observable, TPropertyName extends string & keyof TObservable>(obj: TObservable, prop: TPropertyName) => void) | undefined;
+
+const reactionsToObservables = new WeakTupleSet<[Reaction, Observable, PropertyName]>();
+const observablesToReactions = new WeakTupleSet<[Observable, PropertyName, Reaction]>();
+
+const computationsToObservables = new WeakTupleSet<[Computation, ...unknown[], Observable, PropertyName]>();
+const computationsToCaches = new WeakTupleSet<[Computation, ...unknown[], unknown]>();
+const observablesToComputations = new WeakTupleSet<[Observable, PropertyName, Computation, ...unknown[]]>();
+
+// --- Core functionality ---
+
+function observe<
+    TObservable extends Observable,
+    TPropertyName extends PropertyName & keyof TObservable
+>(
+    obj: TObservable,
+    prop: TPropertyName
+): TObservable[TPropertyName] {
+    const value = obj[prop]
+    reportObservableAccessed?.(obj, prop)
+    return value
+}
+
+function invalidate<
+    TObservable extends Observable,
+    TPropertyName extends PropertyName & keyof TObservable
+>(
+    obj: TObservable,
+    prop: TPropertyName,
+    val: TObservable[TPropertyName]
+) {
+    obj[prop] = val
+
+    for (const [reaction] of observablesToReactions.get([obj, prop])) {
+        reaction()
     }
 
-    return unsubscribe
+    for (const [computation] of observablesToComputations.get([obj, prop])) {
+        computationsToCaches.delete([computation])
+    }
 }
 
-export function when(fn: () => boolean): Promise<void> {
-    // return new Promise(resolve => setTimeout(resolve, 1000))
-    return new Promise(resolve => {
-        function effect() {
-            const conditionMet = fn()
+function autorun(reaction: Reaction) {
 
-            if (conditionMet) {
-                unsubscribe()
-                resolve()
+    function dispose() {
+        for (const [obj, prop] of reactionsToObservables.get([reaction])) {
+            observablesToReactions.delete([obj, prop, reaction])
+        }
+        reactionsToObservables.delete([reaction])
+    }
+
+    const previous = reportObservableAccessed
+    reportObservableAccessed = (obj, prop) => {
+        dispose()
+        reactionsToObservables.add([reaction, obj, prop])
+        observablesToReactions.add([obj, prop, reaction])
+    }
+
+    reaction()
+
+    reportObservableAccessed = previous
+
+    return dispose
+}
+
+function memo<F extends Computation>(fn: F): F {
+    return ((...args) => {
+
+        function dispose() {
+            for (const entry of computationsToObservables.get([fn])) {
+                const obj = entry[entry.length - 2]
+                const prop = entry[entry.length - 1]
+
+                observablesToComputations.delete([obj, prop, fn])
             }
+            computationsToCaches.delete([fn])
+            computationsToObservables.delete([fn])
         }
 
-        Object.defineProperty(effect, 'name', { value: 'when(' + fn.name + ')', writable: false });
-
-        const unsubscribe = autorun(effect, undefined)
-    })
-}
-
-export function action<F extends (...args: any[]) => void>(fn: F): F {
-    return ((...args: unknown[]) => {
-        const topOfAction = queuedReactions == null
-        queuedReactions = queuedReactions ?? new Set()
-
-        fn(...args)
-
-        if (topOfAction) {
-            for (const effect of queuedReactions) {
-                effect()
+        if (!computationsToCaches.has([fn, ...args])) {
+            const previous = reportObservableAccessed
+            reportObservableAccessed = (obj, prop) => {
+                dispose()
+                computationsToObservables.add([fn, ...args, obj, prop])
+                observablesToComputations.add([obj, prop, fn, ...args])
             }
-            queuedReactions = undefined
+
+            const result = fn(...args)
+
+            reportObservableAccessed = previous
+
+            computationsToCaches.add([fn, ...args, result])
         }
-    }) as unknown as F
+
+        for (const [res] of computationsToCaches.get([fn, ...args])) {
+            return res
+        }
+    }) as F
 }
 
-export function runInAction(fn: () => void) {
-    const actionFn = action(fn)
-    actionFn()
-}
 
-export function triggeredBy() {
-    return trigger
-}
+// --- Declare missing TS types ---
 
-export type Plan<T> = () => Promise<T>
+// interface WeakRef<T extends object> {
+//     readonly [Symbol.toStringTag]: "WeakRef";
+
+//     /**
+//      * Returns the WeakRef instance's target object, or undefined if the target object has been
+//      * reclaimed.
+//      */
+//     deref(): T | undefined;
+// }
+
+// interface WeakRefConstructor {
+//     readonly prototype: WeakRef<any>;
+
+//     /**
+//      * Creates a WeakRef instance for the given target object.
+//      * @param target The target object for the WeakRef instance.
+//      */
+//     new <T extends object>(target: T): WeakRef<T>;
+// }
+
+// declare var WeakRef: WeakRefConstructor;
+
+// interface FinalizationRegistry<T> {
+//     readonly [Symbol.toStringTag]: "FinalizationRegistry";
+
+//     /**
+//      * Registers an object with the registry.
+//      * @param target The target object to register.
+//      * @param heldValue The value to pass to the finalizer for this object. This cannot be the
+//      * target object.
+//      * @param unregisterToken The token to pass to the unregister method to unregister the target
+//      * object. If provided (and not undefined), this must be an object. If not provided, the target
+//      * cannot be unregistered.
+//      */
+//     register(target: object, heldValue: T, unregisterToken?: object): void;
+
+//     /**
+//      * Unregisters an object from the registry.
+//      * @param unregisterToken The token that was used as the unregisterToken argument when calling
+//      * register to register the target object.
+//      */
+//     unregister(unregisterToken: object): void;
+// }
+
+// interface FinalizationRegistryConstructor {
+//     readonly prototype: FinalizationRegistry<any>;
+
+//     /**
+//      * Creates a finalization registry with an associated cleanup callback
+//      * @param cleanupCallback The callback to call after an object in the registry has been reclaimed.
+//      */
+//     new <T>(cleanupCallback: (heldValue: T) => void): FinalizationRegistry<T>;
+// }
+
+// declare var FinalizationRegistry: FinalizationRegistryConstructor;
