8d8f459711ff81750b03d956bc833619989cb9c6
Brandon Smith <mail@brandons.me>
2023-04-08 14:28:25 -0500

    Include core.ts and reactivity.ts in bundle

--- src/passes/bundle.rs
+++ src/passes/bundle.rs
@@ -2,9 +2,15 @@ use crate::model::module::ModulesStore;
 use crate::passes::compile::CompileContext;
 use std::fmt::Write;
 
+const CORE_TS: &str = include_str!("../../lib/ts/core.ts");
+const REACTIVITY_TS: &str = include_str!("../../lib/ts/reactivity.ts");
+
 impl ModulesStore {
     pub fn bundle<'a>(&self) -> String {
         let mut buf = String::new();
+
+        buf += CORE_TS;
+        buf += REACTIVITY_TS;
 
         // VERY NAIVE FOR NOW
         for module in self.iter().filter_map(|(_, module)| module.as_ref().ok()) {
